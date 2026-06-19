cddb1f6e32272abc2185321f1855a2b45288d642
Pooja Potdar <poojapotdar10@gmail.com>
2026-06-19 13:51:00 +0900

    commited_advSelBatch practice

---
+++ src/test/java/Demo/TC01_DemoTest.java
@@ -0,0 +1,12 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class TC01_DemoTest 
+{
+	@Test (groups = "smoke")
+	public void movie1test() 
+	{
+		System.out.println("Swapped");
+	}
+}
---
+++ src/test/java/Demo/TC02_Demo1Test.java
@@ -0,0 +1,12 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class TC02_Demo1Test 
+{
+	@Test
+	public void movie1test() 
+	{
+		System.out.println("Nemo");
+	}
+}
